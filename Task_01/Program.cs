eb2e82dae36739e872d0a7cb6d7d6fca843bae5f
xan_so1o <aleksandr.vorobets@gmail.com>
2021-09-02 19:58:10 +0300

    Initial commit

---
+++ Task_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*Develop a program that takes a path to a folder and creates a frequency dictionary of words found in all
+text files of the specified folder. The result of the program is a text file containing
+all words and their number of detections in files, sorted by number
+
+Example:
+mom - 23
+walked - 12
+on - 3
+market -2*/
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter directory path: ");
+            string path = Console.ReadLine();
+            Text txt = new Text(path);
+            Dictionary<string, int> dict = txt.ReadFile();
+            IOrderedEnumerable<KeyValuePair<string, int>> registry = txt.SortDescending(dict);
+            Console.WriteLine("Enter path for file result: ");
+            string fileResult = Console.ReadLine();
+            txt.FileSave(registry, fileResult);
+        }
+    }
+}
---
+++ Task_01/Text.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_01
+{
+    class Text
+    {
+        string ext = ".txt";
+        string path;
+        string[] fileList;
+        Dictionary<string, int> registry = new Dictionary<string, int>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public Text(string path)
+        {
+            this.path = path;
+            this.fileList = Directory.GetFiles(this.path);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, int> ReadFile()
+        {
+            char[] sep = { ' ', '.', ',', ':', ';', '!', '?' };
+            foreach (var file in fileList)
+            {
+                if (file.Contains(ext))
+                {
+                    string[] text = File.ReadAllLines(file);
+                    foreach (var line in text)
+                    {
+                        string[] words = line.Split(sep);
+                        foreach (var word in words)
+                        {
+                            if (registry.ContainsKey(word))
+                            {
+                                registry[word]++;
+                            }
+                            else
+                            {
+                                registry.Add(word, 1);
+                            }
+                        }
+                    }
+                }
+            }
+            return this.registry;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pairs"></param>
+        /// <returns></returns>
+        public IOrderedEnumerable<KeyValuePair<string, int>> SortDescending(Dictionary<string, int> pairs)
+        {
+            return pairs.OrderByDescending(i =>i.Value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="registry"></param>
+        /// <param name="fileResult"></param>
+        public void FileSave(IOrderedEnumerable<KeyValuePair<string, int>> registry, string fileResult)
+        {
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(fileResult + "\\result.txt");
+                foreach (var word in registry)
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(word.Key + " " + word.Value);
+                    streamWriter.WriteLine(word.ToString());
+                }
+                streamWriter.Close();
+                Console.WriteLine("Done!");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception source: {0}", exception.Source);
+            }
+        }
+    }
+        
+}
